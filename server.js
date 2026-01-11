3043146d6539b892a8350a57ca05242df43790ef
wadecalvin9 <102851547+wadecalvin9@users.noreply.github.com>
2026-01-11 17:51:28 +0300

    added ping mechanism for long connections

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const wss = new WebSocket.Server({ port: PORT });
 
 console.log(`WebSocket server running on port ${PORT}`);
 
+function heartbeat() {
+    this.isAlive = true;
+}
+
 wss.on('connection', (ws) => {
     console.log('Client connected');
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+
     ws.send('Welcome to the WebSocket server!');
 
     ws.on('message', (message) => {
@@ -15,4 +22,18 @@ wss.on('connection', (ws) => {
     });
 
     ws.on('close', () => console.log('Client disconnected'));
+});
+
+// Ping all connected clients every 30 seconds to keep the connection alive
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 30000);
+
+wss.on('close', function close() {
+    clearInterval(interval);
 });
